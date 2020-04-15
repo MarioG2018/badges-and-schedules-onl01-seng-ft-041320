6f20b5647a97b1f477171831237f02b75eaf92a0
Mario Garza <mario.garza02@utrgv.edu>
2020-04-15 21:39:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badge_messages=[]
+  attendees.each do |attendee|
+  badge_messages.push("Hello, my name is #{attendee}.")
+  end
+  badge_messages
+end
+
+def assign_rooms(attendees)
+  rooms=[]
+  attendees.each do |room|
+  rooms.push("Hello, #{} your room number is #{room}!")
+  end
+  rooms
+end
