3199f2f6e6cbb5b73a048aa89fa6fe9d1d449207
Mario Garza <mario.garza02@utrgv.edu>
2020-04-15 23:33:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,6 +23,6 @@ def printer(attendees)
   badges=batch_badge_creator(attendees)
   room_assignments=assign_rooms(attendees)
   attendees.each do |badge|
-  puts "Hello, my name is #{badge}. You'll be assigned to room #{room_assignment|"
+  puts "Hello, my name is #{badge}. You'll be assigned to room #{room_assignment}"
   end
 end
