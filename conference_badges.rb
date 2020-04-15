11e00d2b26d49dd41b907b6c80f111bbb8787777
Mario Garza <mario.garza02@utrgv.edu>
2020-04-15 22:28:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   rooms=[]
   attendees.each_with_index do|attendee,room|
-  rooms.push("Hello, #{attendee} your room number is #{room}!")
+  rooms.push("Hello, #{attendee}! You'll be assigned to room  om}!")
   end
   rooms
 end
